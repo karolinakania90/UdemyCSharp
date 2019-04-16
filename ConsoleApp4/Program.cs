e55fa1b9403a9dd9d93caaf0d6716890899a66f4
Karolina Kania <karolina.kania@microsoft.wsei.edu.pl>
2019-04-16 21:14:51 +0200

    Add project files.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Wpisz swoje imię: ");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Witaj, " + name);
+
+            Console.Write("Podaj wiek: ");
+            int age;
+            bool result = int.TryParse(Console.ReadLine(), out age);
+
+            if (age > 18)
+            {
+                Console.WriteLine("Możesz się napić piwa!");
+            }
+            else if (result == false)
+            {
+                Console.WriteLine("Wprowadź poprawny wiek!");
+            }
+            else
+            {
+                Console.WriteLine("Zostań przy mleku :)");
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
