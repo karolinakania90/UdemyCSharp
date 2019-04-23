99402a993aebbe1b964b43f7e3eaf17ac5f02a5c
Karolina Kania <karolina.kania@microsoft.wsei.edu.pl>
2019-04-23 20:53:24 +0200

    added new project - StudentDiary

---
+++ StudentDiary/Diary.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace StudentDiary
+{
+    class Diary
+    {
+        List<float> ratings = new List<float>();
+        public void AddRating(float rating)
+        {
+            ratings.Add(rating);
+        }
+
+        public float CalculateAverage()
+        {
+            float sum = 0, avg = 0; 
+            foreach (var rating in ratings)
+            {
+                sum += rating;
+            }
+
+            avg = sum / ratings.Count();
+
+            return avg;
+        }
+
+        public float GiveMaxRating()
+        {
+            return ratings.Max();
+        }
+
+        public float GiveMinRating()
+        {
+            return ratings.Min();
+        }
+    }
+}
---
+++ StudentDiary/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentDiary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Diary diary = new Diary();
+            //diary.AddRating(5);
+            //diary.AddRating(8.5f);
+            //diary.AddRating(4.7f);
+
+            //float  avg = diary.CalculateAverage();
+            //float max = diary.GiveMaxRating();
+            //float min = diary.GiveMinRating();
+
+            for (; ;)
+            {
+                Console.WriteLine("Podaj ocenę z zakresu 1 - 10");
+
+                float rating;
+                bool result = float.TryParse(Console.ReadLine(), out rating);
+
+                if (rating == 11)
+                {
+                    break;
+                }
+                if (result) 
+                {
+                    diary.AddRating(rating);
+                }
+                
+
+            }
+            Console.WriteLine("Średnia Twoich ocen to: " + diary.CalculateAverage());
+            Console.WriteLine("Najwyższa ocena to: " + diary.GiveMaxRating());
+            Console.WriteLine("Najniższa ocena to: " + diary.GiveMinRating());
+            Console.ReadKey();
+        }
+    }
+}
